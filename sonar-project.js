d941c6be3aea38d8dce76ade0a6063e418216f17
rajasekhar822 <117449780+rajasekhar822@users.noreply.github.com>
2023-01-07 14:14:53 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'squ_3f2373272bebaed6023c4ad03c0985c113eb559',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'password',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
